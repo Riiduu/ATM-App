63e799d12dc5790c2cf00aca5f756a6d8748ab1b
riiduu <jelinsksk@gmail.com>
2022-09-09 21:25:12 +0300

    Created login Function

--- CredentialCheck.cs
+++ CredentialCheck.cs
@@ -2,5 +2,32 @@
 
 public class CredentialCheck
 {
-    
+    public List<String> usernames = new List<string>();
+    public List<String> passwords = new List<string>();
+
+    public void RegisterFunc()
+    {
+        
+    }
+
+    public void LoginFunc()
+    {
+        Console.WriteLine("Enter username");
+        Console.Write("username: ");
+        string username = Console.ReadLine();
+        
+        Console.WriteLine("Enter password");
+        Console.Write("password: ");
+        string password = Console.ReadLine();
+
+        for (int i = 0; i < usernames.Count; i++)
+        {
+            if (username == usernames[i] && password == passwords[i])
+                Console.WriteLine("Login Successful");
+            if (i > 0)
+                continue;
+            
+            Console.WriteLine("Checking...");
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ATMApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CredentialCheck creds = new CredentialCheck();
+        
+        Console.WriteLine("1. Login");
+        Console.WriteLine("2. Register");
+        Console.Write("action >");
+        int action = Int32.Parse(Console.ReadLine());
+
+        creds.usernames.Add("admin");
+        creds.passwords.Add("test");
+        creds.usernames.Add("admin1");
+        creds.passwords.Add("test1");
+        creds.usernames.Add("admin2");
+        creds.passwords.Add("test2");
+
+        if (action == 1)
+            creds.LoginFunc();
+        else
+            creds.RegisterFunc();
+    }
+}
